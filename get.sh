90f4270fe61e0415b0571522f5360feeb4e12bef
polyakpavlo <poliyakpavlo@gmail.com>
2021-02-23 11:02:01 -0500

    log_sftp

--- get.sh
+++ get.sh
@@ -1,45 +1,16 @@
-
-#ls
-#xterm &
-#cat scr.sh
-#ls -la
-#echo "Done"
-#sudo chmod a+x scr.sh
-
-#HOST="192.168.0.103"
-#USER="user"
-#PWD="1"
-#sftp $USER@$HOST <<EOF
-#cd /tmp/
-#lcd /nome/user/
-#get stpd.log log.txt
-#EOF
-
 #!/bin/bash
-sudo yum update
-sudo yum install rsync
-
+set -e
+LOG_F="/tmp/sftp_"`date "+%F-%T"`".log"
+exec &> >(tee "$LOG_F")
+sudo chmod a+x get.sh
+#tar cvf log.tar /tmp/
+#pwd
+#ls -l
 HOST="192.168.0.103"
-NAME="user"
-FILE1="/tmp/*.log"
-FILE2="log.txt"
-sudo apt-get install ftp
-command -v ftp
-sudo yum install ftp
-command -v ftp
-ftp $HOST
-ls
-get $FILE1
-put $FILE1
-ls /home/$NAME
-
-#sftp
-sftp $HOST
-#/home/user/.ssh/known_hosts by default
-scp $FILE1 $NAME@$HOST:$FILE2
-ls
-get $FILE1
-$ put $FILE1
+USER="user"
+PWD="1"
+sftp $USER@$HOST <<EOF
 ls -l
-#scp user@192.168.178.28:file user@192.168.178.27:file
-rsync $FILE1 $NAME@$HOST:$FILE2
+lcd /home/user/
+get $LOG_F logs.txt
+EOF
