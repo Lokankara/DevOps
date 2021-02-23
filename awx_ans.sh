b1a4fecda5b087ac559a0461347d49bd08887e10
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-02-23 11:57:39 +0200

    Update awx_ans.sh

--- awx_ans.sh
+++ awx_ans.sh
@@ -1,14 +1,16 @@
-apt-get update
-apt-get install apt-transport-https wget gnupg python3 python3-pip python-dev tree libpq-dev
-update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-pip3 install ansible
-apt-get install docker.io
-apt-get install npm
-npm install npm --global
-reboot
-pip3 install docker-compose
-mkdir /downloads
-cd /downloads
-git clone https://github.com/ansible/awx.git
-cd /downloads/awx/installer
+yum install -y epel-release
+yum -y install git gcc gcc-c++ nodejs gettext device-mapper-persistent-data lvm2 bzip2 python-pip
+yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+systemctl enable --now docker.service
+pip install docker-compose
+git clone --depth 50 https://github.com/ansible/awx.git
+cd awx/installer
+sed -i 's|admin_password=.*|admin_password=admin@12345|g' inventory
+openssl rand -base64 30
+sed -i 's|secret_key=.*|secret_key=ejv9P72oNvD4AtWLhOUTvpxfdIfKIid3skwuK+ES|g' inventory
+grep -v '^#' inventory | grep -v '^$'
 ansible-playbook -i inventory install.yml
+docker ps
+firewall-cmd --permanent --add-service=http
+firewall-cmd --reload
